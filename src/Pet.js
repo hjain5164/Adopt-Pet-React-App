5df584c776a935fd582571b9373c85381a6d0b7d
Harsh <hjain5164@gmail.com>
2020-05-24 22:40:37 +0530

    Added Eslint Rules and Replaced Code with JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,10 @@
 import React from "react";
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, name),
-    React.createElement("h2", null, animal),
-    React.createElement("h2", null, breed),
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
