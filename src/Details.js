de8b5d1363c10c1b0de5fef863494cdfb24e1e8a
Harsh <hjain5164@gmail.com>
2020-05-26 17:07:41 +0530

    Added Carousel, Details Route and Custom Babel Config

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,50 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{props.id}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     loading: true,
+  //   };
+  // }
+
+  // We can replace it with this by using Babel to parse our code.
+  state = { loading: true };
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name, media } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
